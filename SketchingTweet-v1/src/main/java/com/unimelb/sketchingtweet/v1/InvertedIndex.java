187fcdd6b197a7b572f460a4fd5e3b1620b3666f
jssche <chengsi0607@gmail.com>
2021-08-25 20:50:47 +1000

    added timing code, query code

--- SketchingTweet-v1/src/main/java/com/unimelb/sketchingtweet/v1/InvertedIndex.java
+++ SketchingTweet-v1/src/main/java/com/unimelb/sketchingtweet/v1/InvertedIndex.java
@@ -2,103 +2,129 @@ package com.unimelb.sketchingtweet.v1;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import static java.lang.Math.ceil;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import org.json.*;
+import java.util.concurrent.TimeUnit;
+
+
 
 public class InvertedIndex {
     
-    private List<String> stopwords = Arrays.asList("rt", "a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "couldn", "couldn't", "d", "did", "didn", "didn't", "do", "does", "doesn", "doesn't", "doing", "don", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn", "hadn't", "has", "hasn", "hasn't", "have", "haven", "haven't", "having", "he", "her", "here", "hers", "herself", "him", "himself", "his", "how", "i", "if", "in", "into", "is", "isn", "isn't", "it", "it's", "its", "itself", "just", "ll", "m", "ma", "me", "mightn", "mightn't", "more", "most", "mustn", "mustn't", "my", "myself", "needn", "needn't", "no", "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or", "other", "our", "ours", "ourselves", "out", "over", "own", "re", "s", "same", "shan", "shan't", "she", "she's", "should", "should've", "shouldn", "shouldn't", "so", "some", "such", "t", "than", "that", "that'll", "the", "their", "theirs", "them", "themselves", "then", "there", "these", "they", "this", "those", "through", "to", "too", "under", "until", "up", "ve", "very", "was", "wasn", "wasn't", "we", "were", "weren", "weren't", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "won", "won't", "wouldn", "wouldn't", "y", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", "could", "he'd", "he'll", "he's", "here's", "how's", "i'd", "i'll", "i'm", "i've", "let's", "ought", "she'd", "she'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "we'd", "we'll", "we're", "we've", "what's", "when's", "where's", "who's", "why's", "would");
     private MyHashTable table = null;
     private int numTweet = 0;
+    private ArrayList<String> queryList = new ArrayList<String>();
+    private int indexTimingUnit = 200;
+    private int queryTimingUnit = 200;
     
     
-    private void initialize(){       
-        this.table = new MyHashTable();
-//        System.out.println(this.table.get("xss"));
-//        this.table.put("thank", 0);
-//        System.out.println(this.table.get("thank"));
-//        this.table.put("thank", 52);
-//        System.out.println(this.table.get("thank").getKey());
-        
+    
+    private void initialize(String queryTweetFile, String indexTweetFile){     
+
+        int counter = 0;  
         Scanner inputStream = null; 
+        this.table = new MyHashTable();
+
+        // construct query list
         try{
-            inputStream = new Scanner(new FileInputStream("tweets.txt"));
-        }
-        catch(FileNotFoundException e){
-            System.out.println("tweets.txt was not found");
-        }
-        while(inputStream.hasNextLine()){
-            String line = inputStream.nextLine();
-            this.addTweet(line);
-        } 
-//        System.out.println(this.table.getSize());
-//        System.out.println(this.table.get("thank"));
-    }
-    
-    
-    private void addTweet(String line){
-        String tweetID = null;
-        ArrayList<String> textList = null;
-         
-        try {
-            JSONObject jsonLine = new JSONObject(line);
-            tweetID = jsonLine.getString("id_str");            
-            textList = this.cleanTweet(jsonLine.getString("text"));            
-            if(!textList.isEmpty()){
-                this.numTweet++;
-                Pair tweet = new Pair(numTweet, textList);
-//                System.out.println(tweetID);
-                System.out.println(tweet);
-                for(int i=0;i<textList.size();i++){
-                    this.table.put(textList.get(i), numTweet);
+            inputStream = new Scanner(new FileInputStream(queryTweetFile));
+            while(inputStream.hasNextLine()){
+                String line = inputStream.nextLine();
+                if(line!=""){
+                    queryList.add(line);
                 }
             }
-            textList.clear();
-        } catch (JSONException e) {
-//            System.out.println("not a json file");
+        }
+        catch(FileNotFoundException e){
+            System.out.println("file not found");
+        }
+  
+        // construct hash table
+        try{
+            inputStream = new Scanner(new FileInputStream(indexTweetFile));
+            long startTime = System.nanoTime();
+            while(inputStream.hasNextLine()){
+                String line = inputStream.nextLine();
+                if(line!=""){
+                    this.addTweetToTable(line);
+                }
+                // counter++;
+                // if(counter == this.indexTimingUnit){
+                //     long currentTime = System.nanoTime();
+                //     long timeElapsed = currentTime - startTime;
+                //     System.out.println(timeElapsed * 1.0 / 1000000.0); // milliseconds
+                //     startTime = currentTime;
+                //     counter = 0;
+                // }
+            } 
+        }
+        catch(FileNotFoundException e){
+            System.out.println("file not found");
+        }
+        // System.out.println(this.queryList);
+        // System.out.println(this.queryList.size());
+    }
+
+
+    private void writeTweet(ArrayList<List<String>> tweets, String fileName){
+        String tweetsString = "";
+
+        for(int i=0; i<tweets.size();i++){
+            tweetsString = tweetsString + tweets.get(i) + "\n";
+        }
+
+        try {
+            FileWriter myWriter = new FileWriter(fileName);
+            myWriter.write(tweetsString);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
     
-    
-    private ArrayList<String> cleanTweet(String text){
-        String[] textArray = null;
-        ArrayList<String> textList = new ArrayList();
-        text = text.replaceAll("(@[A-Za-z0-9_-]+)|(http\\S+)|(https\\S+)|[^a-zA-Z0-9\\s]|[\\n\\t\\r]","")
-                    .toLowerCase().trim();
-        textArray = text.split(" ",0);
-        for(int i=0; i<textArray.length; i++){
-            if(!textArray[i].isBlank()){
-                textList.add(textArray[i].trim());
+   
+    private void addTweetToTable(String line){
+        List<String> termList = null;
+        termList = Arrays.asList(line.split(",",0));
+         
+        if(termList.size() != 0){
+            this.numTweet++;
+            // System.out.println(numTweet);
+            // System.out.println(termList);
+            for(int i=0;i<termList.size();i++){
+                this.table.put(termList.get(i).trim(), numTweet);
             }
         }
-        textList.removeAll(this.stopwords);
-        return textList;
     }
     
     
-    public String findSimilarTweet(String newTweet, double similarity){
+    private String findSimilarTweet(String query, double similarityThreshold){
 //      get the document id list for each of the word in the new tweet
         int totalWords;
-        ArrayList<String> newTweetList = new ArrayList();
-        
-        newTweetList = this.cleanTweet(newTweet);
-        System.out.println(newTweetList);
-        totalWords = newTweetList.size();
+        List<String> termList;
+
+        termList = Arrays.asList(query.split(",", 0));
+        // System.out.println(termList);
+        totalWords = termList.size();
         
         MyHashNode[] docLists = new MyHashNode[totalWords];
         for(int i=0;i<totalWords;i++){
-            docLists[i] = this.table.getDocList(newTweetList.get(i));
+            docLists[i] = this.table.getDocList(termList.get(i).trim());
 //            System.out.println(docLists[i]);
         }
-        
+
 //      linear search
         int latestDocID = 0;
         ArrayList<Integer> latestDocIndex = new ArrayList<Integer>();
-        int threshold = (int)ceil(similarity * totalWords);
+        int threshold = (int)ceil(similarityThreshold * totalWords);
 //        System.out.println(threshold);
         int nullCount = 0;
          
@@ -123,32 +149,56 @@ public class InvertedIndex {
 //          if yes, return the doc id
 //          if no, move the pointers of the words that have the max doc id to the next doc id and restores the maxDocID
             if(nullCount == totalWords){
-                return "No similar tweet";
+                // System.out.println("Similarity: 0 \nNo similar tweet found");
+                return "-1";
             }else{
                 if(latestDocIndex.size() >= threshold){
+                    double similar = (latestDocIndex.size() * 1.0)/(totalWords * 1.0);
+                    // System.out.println("Similarity: " + String.format("%.5f", similar) +"\n" + "The most similar doc id: " + Integer.toString(latestDocID));
                     return Integer.toString(latestDocID);
                 }else{
                     for(int i=0;i<latestDocIndex.size();i++){
                         docLists[latestDocIndex.get(i)] = docLists[latestDocIndex.get(i)].getNext();
-                    }
-                    latestDocIndex.clear();
-                    latestDocID = 0;
-                    nullCount = 0;
+                }
+                latestDocIndex.clear();
+                latestDocID = 0;
+                nullCount = 0;
                 }
             }              
-        }        
+        }
     }
-    
-    
+
+    private void runQueryList(ArrayList<String> queryList, double threshold){
+        long startTime = System.nanoTime();
+
+        for(int i=0;i<this.queryList.size();i++){
+            // System.out.println("Query id " + Integer.toString(i+1));
+            this.findSimilarTweet(this.queryList.get(i), 0.5);
+
+            if(i % this.queryTimingUnit == 0){
+                long currentTime = System.nanoTime();
+                long timeElapsed = currentTime - startTime;
+                System.out.println(timeElapsed * 1.0 / 1000000.0); // milliseconds
+                startTime = currentTime;
+            }
+        }
+    }
+
+
     public static void main(String[] arg){
         InvertedIndex fileIndex = new InvertedIndex();
-        fileIndex.initialize();
-        // System.out.println(fileIndex.table.getTableNode("latest").getCount());
+        fileIndex.initialize("tweets_querying.txt", "tweets_indexing.txt");
+        fileIndex.runQueryList(fileIndex.queryList, 0.5);
+
         
-        System.out.println("doc id: " + fileIndex.findSimilarTweet("sxx sxx xxx", 0.5));
-        System.out.println("doc id: " + fileIndex.findSimilarTweet("Latest level: 0.8815m at 31/03/2016 23:00:00(GMT). Further information available at https://t.co/Jy7C3IU3nQ #riverlevels", 0.5));
-        System.out.println("doc id: " + fileIndex.findSimilarTweet("spend more time trying to change someone and less time trying to love and understand them xxx", 0.5));
-        System.out.println("doc id: " + fileIndex.findSimilarTweet("Get Weather Updates from The Weather Channel", 1));
-        // to do: output the similarity as well
+        
+
+
+        // System.out.println(fileIndex.table.getTableNode("latest").getCount());
+        // System.out.println(fileIndex.findSimilarTweet("sxx sxx xxx", 0.5));
+        // System.out.println(fileIndex.findSimilarTweet("Latest level: 0.8815m at 31/03/2016 23:00:00(GMT). Further information available at https://t.co/Jy7C3IU3nQ #riverlevels", 0.5));
+        // System.out.println(fileIndex.findSimilarTweet("spend more time trying to change someone and less time trying to love and understand them xxx", 0.5));
+        // System.out.println(fileIndex.findSimilarTweet("Get Weather Updates from The Weather Channel", 1));
+        
     }
 }
--- preprocessing/src/main/java/com/example/App.java
+++ preprocessing/src/main/java/com/example/App.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +16,8 @@ public class App
 {
     private List<String> stopwords = Arrays.asList("rt", "a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "couldn", "couldn't", "d", "did", "didn", "didn't", "do", "does", "doesn", "doesn't", "doing", "don", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn", "hadn't", "has", "hasn", "hasn't", "have", "haven", "haven't", "having", "he", "her", "here", "hers", "herself", "him", "himself", "his", "how", "i", "if", "in", "into", "is", "isn", "isn't", "it", "it's", "its", "itself", "just", "ll", "m", "ma", "me", "mightn", "mightn't", "more", "most", "mustn", "mustn't", "my", "myself", "needn", "needn't", "no", "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or", "other", "our", "ours", "ourselves", "out", "over", "own", "re", "s", "same", "shan", "shan't", "she", "she's", "should", "should've", "shouldn", "shouldn't", "so", "some", "such", "t", "than", "that", "that'll", "the", "their", "theirs", "them", "themselves", "then", "there", "these", "they", "this", "those", "through", "to", "too", "under", "until", "up", "ve", "very", "was", "wasn", "wasn't", "we", "were", "weren", "weren't", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "won", "won't", "wouldn", "wouldn't", "y", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", "could", "he'd", "he'll", "he's", "here's", "how's", "i'd", "i'll", "i'm", "i've", "let's", "ought", "she'd", "she'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "we'd", "we'll", "we're", "we've", "what's", "when's", "where's", "who's", "why's", "would");
     private ArrayList<String> tweets = new ArrayList<String>(); 
+    private ArrayList<String> indexedTweetList = new ArrayList<String>();
+    private ArrayList<String> queryList = new ArrayList<String>();
 
     private int getTweetsCount(){
         return this.tweets.size();
@@ -118,11 +121,54 @@ public class App
         
     }
 
+    private void divideIndexQueryTweets(String filename){
+        Scanner inputStream = null; 
+        Random rand = new Random();
+        rand.setSeed(5);
+
+        try{
+            inputStream = new Scanner(new FileInputStream(filename));
+        }
+        catch(FileNotFoundException e){
+            System.out.println("file not found");
+        }
+
+        while(inputStream.hasNextLine()){
+            String line = inputStream.nextLine();
+            if(line!=""){
+                if(rand.nextInt(10)<9){
+                    this.indexedTweetList.add(line + "\n");
+                }else{
+                    this.queryList.add(line + "\n");
+                }
+            }
+        }
+    }
+
+    // private void addTweetToQueryList(String line){
+    //     List<String> termList = null;
+    //     termList = Arrays.asList(line.split(",",0));
+    //     if(termList.size() != 0){
+    //         this.queryList.add(termList);
+    //     }
+    // }
+
+    // private void addTweetToIndexedList(String line){
+    //     List<String> termList = null;
+    //     termList = Arrays.asList(line.split(",",0));
+    //     if(termList.size() != 0){
+    //         this.indexedTweetList.add(termList);
+    //     }
+    // }
+
     public static void main( String[] args )
     {   
         App prerocessor = new App();
-        final File folder = new File("C:\\Users\\cheng\\Documents\\Study\\Research Project\\RawData");
-        prerocessor.processFilesForFolder(folder);
-        prerocessor.sampleTweets(prerocessor.getTweetsCount() * 0.1);
+        // final File folder = new File("C:\\Users\\cheng\\Documents\\Study\\Research Project\\RawData");
+        // prerocessor.processFilesForFolder(folder);
+        // prerocessor.sampleTweets(prerocessor.getTweetsCount() * 0.1);
+        prerocessor.divideIndexQueryTweets("sampledTweets.txt");
+        prerocessor.writeTweet(prerocessor.indexedTweetList, "tweets_indexing.txt");
+        prerocessor.writeTweet(prerocessor.queryList, "tweets_querying.txt");
     }
 }
